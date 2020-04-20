0e971f384a9623d65c34d82a9a949d97fd3a49ec
fedgut <fedgut@gmail.com>
2020-04-20 10:33:49 -0500

    Create basic project set up

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,31 @@
 import sum from './sum';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  const total = sum(1, 2);
+  expect(total).toBe(3);
 });
+
+test('adding positive numbers aint 0', () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(sum(a, b)).not.toBe(0);
+    }
+  }
+});
+
+test('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).not.toBeUndefined();
+  expect(n).toBeDefined();
+  expect(n).toBeFalsy();
+  expect(n).not.toBeTruthy();
+});
+
+test('cero', () => {
+  const z = 0;
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).toBeFalsy();
+  expect(z).not.toBeTruthy();
+})
